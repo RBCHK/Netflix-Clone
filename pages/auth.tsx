cae100538b45f702fbe892981edf897290494449
Artem Rybachuk <artem.rybachuk@gmail.com>
2024-01-18 23:21:30 +0200

    auth pages created

---
+++ pages/auth.tsx
@@ -0,0 +1,10 @@
+const Auth = () => {
+
+    return (
+        <div className='relative h-full w-full bg-[url("/images/hero.jpg")]'>
+            test
+        </div>
+    )
+}
+
+export default Auth
