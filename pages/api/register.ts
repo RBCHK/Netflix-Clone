eb22273a8b83738d032c7636bcc271a389e2f6e3
Artem Rybachuk <artem.rybachuk@gmail.com>
2024-01-25 17:33:20 +0200

    min fix

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,10 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '../../lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') return res.status(405).end()
-    //if (req.method !== 'POST') {
-    //    return res.status(405).end()
-    //}
+    if (req.method !== 'POST') {
+        return res.status(405).end()
+    }
 
     try {
         const { name, email, password } = req.body;
@@ -17,10 +16,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         });
 
-        if (existingUser) return res.status(422).json({error: 'Email taken'})
-        //if (existingUser) {
-        //    return res.status(422).json({error: 'Email taken'})
-        //}
+        if (existingUser) {
+            return res.status(422).json({error: 'Email taken'})
+        }
 
         const hashedPassword = await bcrypt.hash(password, 12)
 
