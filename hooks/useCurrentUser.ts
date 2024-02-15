c8c8a5bb35ee7b735dc1ac7c3c2ef3f347d438f5
Artem Rybachuk <artem.rybachuk@gmail.com>
2024-02-15 17:51:44 +0200

    added Profile page. Pages have been protected

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,17 @@
+// in this case we don't need any state manager for fetcing a user
+import useSWR from 'swr';
+
+import fetcher from '@/lib/fetcher';
+
+const useCurrentUser = () => {
+    const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate,
+    }
+}
+
+export default useCurrentUser
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+
+//Routes protecting function
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context)
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            }
+        }
+    } 
+
+    return {
+        props: {}
+    }
+}
+
 export default function Home() {
-  return (
+    const { data: user } = useCurrentUser()
+    
+    return (
     <>
-      <h1 className="text-1xl text-green-500">Netflix Clone</h1>
+            <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+            <p className='text-white'>Logged in as user: {user?.email}</p>
+          <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
     </>
   )
 }
