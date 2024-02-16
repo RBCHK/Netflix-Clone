5244397935da95dd56ee338f1f5a660d3fa90a0b
Artem Rybachuk <artem.rybachuk@gmail.com>
2024-02-16 18:57:18 +0200

    implemented Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,5 +22,36 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {    
-    return <Navbar />
+    return (
+        <>
+            <Navbar />
+            <div className='bg-gray-500'>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+                <div className='h-96'></div>
+            </div>
+        </>
+    )
 }
