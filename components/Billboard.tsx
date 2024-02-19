31988d6bc2b13d75e85c18fe9f03232ab9bf6265
Artem Rybachuk <artem.rybachuk@gmail.com>
2024-02-19 21:03:20 +0200

    Billboard with error

---
+++ components/Billboard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useBillboard from '@/hooks/useBillboard';
+
+const Billboard = () => {
+    //const { data } = useBillboard();
+    
+    return (
+        <div className=' relative h-[56.25vw]'>
+            <video
+                autoPlay
+                loop
+                muted
+                //poster={data?.thumbnailUrl}
+                //src={data?.videoUrl}
+            >
+            </video>
+        </div>
+    )
+}
+
+export default Billboard
